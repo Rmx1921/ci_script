38b0ce3e2e572f4ece034f7801cb6aa02d95a8a6
Rmx1921 <koreanplay14340@gmail.com>
2022-12-03 12:32:14 +0530

    gg5-4

--- ci_build.sh
+++ ci_build.sh
@@ -57,7 +57,7 @@ Compiler: <code>${COMPILER}</code>
 Branch: <code>$(git rev-parse --abbrev-ref HEAD)</code>
 Commit: <code>$MESSAGE</code>
 <i>Build started on Drone Cloud...</i>
-Check the build status here: https://cloud.drone.io/viciouspup/kernel_realme_sdm710/${DRONE_BUILD_NUMBER}" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
+Check the build status here: https://cloud.drone.io/viciouspup/kernel_realme_sdm710/${DRONE_BUILD_NUMBER}" -d chat_id=338913217 -d parse_mode=HTML
 curl -s -X POST https://api.telegram.org/bot1445481247:AAFmjxDbbXAEFjAgYdyeVj6ZKAq-obPV_64/sendMessage -d text="Build started for revision ${DRONE_BUILD_NUMBER}" -d chat_id=338913217 -d parse_mode=HTML
 
 START=$(date +"%s")
