3aebe56cc585b50f477377e053034800a375cb7e
Rmx1921 <koreanplay14340@gmail.com>
2022-12-03 12:48:52 +0530

    gg5-5

--- ci_build.sh
+++ ci_build.sh
@@ -51,13 +51,13 @@ fi
 # Post to CI channel
 #curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendPhoto -d photo=https://github.com/UtsavBalar1231/xda-stuff/raw/master/banner.png -d chat_id=${CI_CHANNEL_ID}
 curl -s -X POST https://api.telegram.org/bot1445481247:AAFmjxDbbXAEFjAgYdyeVj6ZKAq-obPV_64/sendMessage -d text="<code>SPIRAL</code>
-Build: <code>${TYPE}</code>
+Build: <code>Release</code
 Device: <code>Realme XT(RMX1921)</code>
 Compiler: <code>${COMPILER}</code>
 Branch: <code>$(git rev-parse --abbrev-ref HEAD)</code>
 Commit: <code>$MESSAGE</code>
 <i>Build started on Drone Cloud...</i>
-Check the build status here: https://cloud.drone.io/viciouspup/kernel_realme_sdm710/${DRONE_BUILD_NUMBER}" -d chat_id=338913217 -d parse_mode=HTML
+Check the build status here: https://app.circleci.com/pipelines/github/Rmx1921/spiral_sdm710?branch=main" -d chat_id=338913217 -d parse_mode=HTML
 curl -s -X POST https://api.telegram.org/bot1445481247:AAFmjxDbbXAEFjAgYdyeVj6ZKAq-obPV_64/sendMessage -d text="Build started for revision ${DRONE_BUILD_NUMBER}" -d chat_id=338913217 -d parse_mode=HTML
 
 START=$(date +"%s")
